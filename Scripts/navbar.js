693a4e434e25031953e5f47c364b1475c91b00de
unknown <mhaseeb1604@gmail.com>
2024-01-21 20:06:16 +0500

    Event Page set

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -6,9 +6,7 @@ showpage = () =>{
 
 window.onload = () =>{
     showpage()
-    let navbar = document.getElementById("fixedNav")
     let toggler = document.querySelector(".navbar-toggler")
-    let brand = document.querySelector('.brand-img')
     let scroll = document.querySelector(".scroll-up-btn")
 
     let sections = document.querySelectorAll("section")
@@ -16,13 +14,9 @@ window.onload = () =>{
 
     window.onscroll = () =>{
         if(window.pageYOffset > 0) {
-            navbar.classList.add("sticky")
             scroll.classList.add("show")
-            brand.src = '../Assets/msp-cusit-white.svg'
         }else{
-            navbar.classList.remove("sticky")
             scroll.classList.remove("show")
-            brand.src = '../Assets/msp-cusit.svg'
         }
 
         let current = ''
